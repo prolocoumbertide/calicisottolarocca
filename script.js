eea5bf6853adf2bfd41a8d24c8ab6386bcab4248
prolocoumbertide <prolocoumbertide@gmail.com>
2024-08-01 14:09:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 const saltafilaLink = document.getElementById('saltafila-link');
 const contactsSection = document.getElementById('contacts');
 
-saltafilaLink.addEventListener('click', () => {
-  contactsSection.classList.add('highlight');
+if (saltafilaLink && contactsSection) {
+  saltafilaLink.addEventListener('click', () => {
+    contactsSection.classList.add('highlight');
 
-  const scrollOffset = contactsSection.offsetTop;
-  window.scrollTo({
-    top: scrollOffset,
-    behavior: 'smooth'
+    // Add smooth scrolling
+    const scrollOffset = contactsSection.offsetTop;
+    window.scrollIntoView({
+      top: scrollOffset,
+      behavior: 'smooth'
+    });
+
+    // Remove highlight class after animation is complete
+    requestAnimationFrame(() => {
+      contactsSection.classList.remove('highlight');
+    });
   });
-
-  setTimeout(() => {
-    contactsSection.classList.remove('highlight');
-  }, 1000);
-});
+}
