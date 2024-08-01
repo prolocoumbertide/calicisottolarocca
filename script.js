f31e920c40be01889c8c3bfbfc140c5edf5de27f
prolocoumbertide <prolocoumbertide@gmail.com>
2024-08-01 15:30:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,36 @@
 function smoothScrollToContacts() {
-            const contactsSection = document.getElementById('contacts');
-            if ('scrollBehavior' in document.documentElement.style) {
-                // For modern browsers that support scrollBehavior
-                contactsSection.scrollIntoView({
-                    behavior: 'smooth'
-                });
-            } else {
-                // For older browsers and mobile browsers that don't support scrollBehavior
-                const startY = window.scrollY;
-                const endY = contactsSection.offsetTop;
-                const distance = endY - startY;
-                const duration = 500; // adjust the duration to your liking
-                const startTime = performance.now();
-                function animateScroll() {
-                    const currentTime = performance.now();
-                    const elapsed = currentTime - startTime;
-                    const progress = elapsed / duration;
-                    const newY = startY + (distance * progress);
-                    window.scrollTo(0, newY);
-                    if (progress < 1) {
-                        requestAnimationFrame(animateScroll);
-                    }
-                }
-                animateScroll();
+    const contactsSection = document.getElementById('contacts');
+    if ('scrollBehavior' in document.documentElement.style) {
+        // For modern browsers that support scrollBehavior
+        contactsSection.scrollIntoView({
+            behavior: 'smooth'
+        });
+    } else {
+        // For older browsers and mobile browsers that don't support scrollBehavior
+        const startY = window.scrollY;
+        const endY = contactsSection.offsetTop;
+        const distance = endY - startY;
+        const duration = 500; // adjust the duration to your liking
+        const startTime = performance.now();
+        function animateScroll() {
+            const currentTime = performance.now();
+            const elapsed = currentTime - startTime;
+            const progress = elapsed / duration;
+            const newY = startY + (distance * progress);
+            window.scrollTo(0, newY);
+            if (progress < 1) {
+                requestAnimationFrame(animateScroll);
             }
         }
+        animateScroll();
+    }
+    highlightContactsSection();
+}
+
+function highlightContactsSection() {
+    const contactsSection = document.getElementById('contacts');
+    contactsSection.classList.add('highlight');
+    setTimeout(() => {
+        contactsSection.classList.remove('highlight');
+    }, 1000);
+}
